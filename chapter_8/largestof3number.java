6b0ff6327ea02557026ceaf13e959545d434c00d
vasradinesh <dbvasra32@gmail.com>
2024-02-12 12:39:12 +0530

    largest of 3 numberfile  is added

---
+++ chapter_8/largestof3number.java
@@ -0,0 +1,21 @@
+package chapter_8;
+
+
+public class largestof3number {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 6;
+        if(a>=b && a>=c){
+            System.out.println("a is largest number");
+        }
+        else if(b>=c){
+            System.out.println("b is largest number");
+        }
+        else{
+            System.out.println("c is largest number");
+
+        }
+    }
+    
+}
