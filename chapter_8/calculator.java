d2ef22f39e89b43b71c725c20767bc96517cad62
vasradinesh <dbvasra32@gmail.com>
2024-02-12 15:45:00 +0530

    calculator  filr is added in chapter 8

---
+++ chapter_8/calculator.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char operator = sc.next().charAt(0);
+
+        switch (operator) {
+            case '+': System.out.println(a+b);
+                break;
+            case '-': System.out.println(a-b);
+                break;
+            case '*': System.out.println(a*b);
+                break;
+            case '/': System.out.println(a/b);
+                break;
+            case '%': System.out.println(a%b);
+                break;          
+            default:
+                break;
+        }
+    }
+}
