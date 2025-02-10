cb53d9f0672c01ecde85de3c61f0393645080734
vasradinesh <dbvasra32@gmail.com>
2025-02-10 13:53:54 +0530

    function remove cycle is added in llcreate

--- chapter_23/LLCreate.java
+++ chapter_23/LLCreate.java
@@ -1,4 +1,6 @@
+import java.nio.file.NotDirectoryException;
 
+import org.w3c.dom.Node;
 
 public class LLCreate {
     public static class Node{
@@ -193,14 +195,49 @@ public class LLCreate {
         return false;
     }
 
+    public static void removeCycle(){
+        Node slow = head;
+        Node fast = head;
+        Boolean cycle = false;
+
+        while (fast!=null && fast.next!=null) {
+            slow = slow.next;  // +1
+            fast = fast.next.next; //+2
+
+            if (fast ==slow) {
+                cycle = true;
+                break ; //cycle exist
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+        // finding meeting point
+        slow = head;
+        Node prev = null;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // remove cycle
+     if (prev != null) {
+        prev.next = null;  // Remove cycle
+        }
+    }
+
     public static void main(String[] args){
 
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head ;
+        head.next.next.next = temp ;
 
         System.out.println(iscycle());
+        removeCycle();
+        System.out.println(iscycle());
+
 
         // LLCreate ll = new LLCreate();
         // // ll.head = new Node(1);
