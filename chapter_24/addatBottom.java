136b6716095294f16749f89f2cb27c56ea1e533f
vasradinesh <dbvasra32@gmail.com>
2025-02-20 10:20:18 +0530

    function reverse stack is added in to the addatbottom file

--- chapter_24/addatBottom.java
+++ chapter_24/addatBottom.java
@@ -1,3 +1,4 @@
+import java.security.PublicKey;
 import java.util.*;
 public class addatBottom {
     public static void addAtBottom(Stack<Integer> s , int data){
@@ -10,15 +11,26 @@ public class addatBottom {
         s.push(top);
     }
 
+    public static void ReverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        ReverseStack(s);
+        addAtBottom(s, top);
+    }
+    public static void print(Stack<Integer> s){
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-        addAtBottom(s, 0);
-
-        while (!s.isEmpty()) {
-            System.out.println(s.pop());
-        }
+        ReverseStack(s);
+        print(s);
     }
 }
