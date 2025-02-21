af52c9232a8557bde1723e72ea0cc60bb79b0d22
vasradinesh <dbvasra32@gmail.com>
2025-02-21 23:03:52 +0530

    function isduplicate is added into the file paranthesis

--- chapter_24/paranthesis.java
+++ chapter_24/paranthesis.java
@@ -29,8 +29,36 @@ public class paranthesis {
             return false;
         }
     }
+
+    public static boolean isDuplicate(String str2){
+        Stack<Character> s = new Stack<>();
+
+        for (int i=0;i<str2.length();i++){
+            char ch = str2.charAt(i);
+
+            // closing 
+            if(ch==')'){
+                int count = 0;
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if(count<1){
+                    return true;
+                }else{
+                    s.pop();
+                }
+            }
+            else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         String str = "{{]}}"; 
+        String str2 = "((a+b))";
+        System.out.println(isDuplicate(str2));
         System.out.println(isValid(str));
     }
 }
